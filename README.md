3442ef5fc0693521823b50ac911bf6ccb45ab88e
AleCrypto <alecrypto@gmail.com>
2015-10-24 15:46:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+NewCoin
+====
+
+NewCoin "NEWC" is an X13 PoW/PoS Cryptocurrency.
+
+Algorithm: X13 POW/POS
+
+Ticker: NEWC
+
+Current Version "1.0.0.0"
+
+
+
+==========================================================
+
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define newcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "newcoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
